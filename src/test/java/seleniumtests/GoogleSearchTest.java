0ef2341f373c410b63e942dc9cd7ef0914b52d75
Cristobal Contreras <ccontreras.cumin@gmail.com>
2023-06-20 14:08:40 -0400

    test/Replaced firefox with chrome and modified selenium test.

--- src/test/java/seleniumtests/GoogleSearchTest.java
+++ src/test/java/seleniumtests/GoogleSearchTest.java
@@ -5,9 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
@@ -16,17 +15,10 @@ public class GoogleSearchTest {
 
     @Test
     public void googleSearch() {
-        // Define a path to the geckodriver executable that is required by Firefox
-    	System.setProperty("webdriver.gecko.driver", "./geckodriver.exe");
-    	System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"./geckodriver.log");
+    	ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
 
-    	FirefoxOptions options = new FirefoxOptions();
-    	options.setLogLevel(FirefoxDriverLogLevel.TRACE);
-    	
-    	// Aquí ajusta la ruta a la ubicación de tu instalación de Firefox
-    	options.setBinary("C:\\Program Files\\Firefox Developer Edition\\\\firefox.exe");
-
-    	WebDriver driver = new FirefoxDriver(options);
+        WebDriver driver = new ChromeDriver(options);
 
         // Go to the Google home page
         driver.get("https://www.google.com");
@@ -35,17 +27,17 @@ public class GoogleSearchTest {
         WebElement element = driver.findElement(By.name("q"));
 
         // Enter a search query
-        element.sendKeys("OpenAI");
+        element.sendKeys("Kibernum");
 
         // Submit the query. WebDriver will find the form for us from the element
         element.submit();
-        
+
         // Wait for the page title to be updated
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));  // wait up to 10 seconds
-        wait.until(ExpectedConditions.titleContains("OpenAI"));
-        
+        wait.until(ExpectedConditions.titleContains("Kibernum"));
+
         // Check that the title contains the search query
-        Assertions.assertTrue(driver.getTitle().contains("OpenAI"));
+        Assertions.assertTrue(driver.getTitle().contains("Kibernum"));
 
         driver.quit();
     }
